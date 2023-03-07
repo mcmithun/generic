a02364a6f678dbe9f802ddab3be474040d2743cc
mczachmat <mczachmat@gmail.com>
2023-03-06 19:35:07 -0500

    navi update in 16 ms

--- QTThreadTest/navi.cpp
+++ QTThreadTest/navi.cpp
@@ -9,11 +9,11 @@ Navi::Navi(Subscription<Navi> *sub) : m_sub(sub)
 
 void Navi::Start()
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 10000; i++)
     {
         qDebug()<<"this is navi" << i <<"\n";
         m_data = i;
         m_sub->notify(this);
-        sleep(1);
+        usleep(16000);
     }
 }
